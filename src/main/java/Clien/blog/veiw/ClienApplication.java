3dbaa8935bdcc2cf45e3ec30c82bb3a6411ed39a
Nyftheart <56402047+Nyftheart@users.noreply.github.com>
2021-12-14 16:48:38 +0100

    Add files via upload

---
+++ src/main/java/Clien/blog/veiw/ClienApplication.java
@@ -0,0 +1,13 @@
+package Clien.blog.veiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienApplication.class, args);
+	}
+
+}
