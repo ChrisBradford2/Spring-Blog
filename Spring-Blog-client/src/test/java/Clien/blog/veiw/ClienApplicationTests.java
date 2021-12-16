eadcfac033e9c862f7b52b7ba29eeea47410e010
Nyftheart <56402047+Nyftheart@users.noreply.github.com>
2021-12-16 16:38:29 +0100

    creation session user
    ajout :
    3 page html
    ajout de session 
    ajout login

---
+++ Spring-Blog-client/src/test/java/Clien/blog/veiw/ClienApplicationTests.java
@@ -0,0 +1,13 @@
+package Clien.blog.veiw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
